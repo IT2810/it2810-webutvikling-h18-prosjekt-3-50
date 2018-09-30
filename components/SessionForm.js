5c1cf97b5a8c76a71deff355dc0cbf7b54c21c2d
Dora Oline <doraoline@gmail.com>
2018-09-30 12:33:49 +0200

    Improving design

--- components/SessionForm.js
+++ components/SessionForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
 
-import  { Button, Container, Content, DatePicker, Form, Header, Icon, Input, Item, Label, List, ListItem, H1, H2} from 'native-base'
+import  { Button, Text, View, Container, Content, DatePicker, Form, Card, Footer, FooterTab, ScrollView, Header, Right, Left, Row, Toast, Icon, Input, Item, Label, List, ListItem, H1, H2} from 'native-base'
 
 import AddExercise from './AddExercise.js'
 import ExerciseListEl from './ExerciseListEl.js'
@@ -16,14 +16,19 @@ class SessionForm extends Component {
     this.state = {
       date: this.props.date | null,
       name: null,
-      exercises: null
+      exercises: [
+        {name: 'Squat', sets: '4', reps: '12'},
+        {name: 'Benchpress', sets: '4', reps: '12'},
+        {name: 'Pullup', sets: '4', reps: '12'},
+        {name: 'Row', sets: '4', reps: '12'}
+      ],
+      showToast: false
     }
 
     this.setDate = this.setDate.bind(this)
     this.saveSession = this.saveSession.bind(this)
     this.validateSession = this.validateSession.bind(this)
   }
-
 
   setDate(newDate) {
     this.setState({ date: newDate })
@@ -32,6 +37,8 @@ class SessionForm extends Component {
   saveSession() {
     if (this.validateSession()) {
       // TODO save in state with redux
+
+      this.props.navigation.navigate('Home')
     }
   }
 
@@ -54,8 +61,13 @@ class SessionForm extends Component {
   showToast(text) {
     Toast.show({
       type: 'danger',
+      position: 'top',
       duration: 3000,
-      text: text
+      text: text,
+      style: {
+        margin: 8,
+        padding: 16
+      }
     })
   }
 
@@ -64,56 +76,63 @@ class SessionForm extends Component {
 
     return (
       <Container>
-        <Content>
-          <Form>
-            <Item floatingLabel>
-              <Label> Name </Label>
-              <Input 
-                onChangeText={(name) => this.setState({name})}
-                value={this.state.name}
-              />
-            </Item>
+        <Content padder>
+          <Item floatingLabel>
+            <Label> Name </Label>
+            <Input 
+              onChangeText={(name) => this.setState({name})}
+              value={this.state.name}
+            />
+          </Item>
 
-            <Item>
-              <Label> Date </Label>
-              <DatePicker
-                androidMode={"default"}
-                placeHolderText="Select date"
-                onDateChange={(date) => this.setDate({date})}
-                value={this.state.date}
-              />
-            </Item>
+          <Item>
+            <Label> Date </Label>
+            <DatePicker
+              androidMode={"default"}
+              placeHolderText="Select date"
+              onDateChange={(date) => this.setDate({date})}
+              value={this.state.date}
+            />
+          </Item>
 
-            <List>
-              <ListItem itemHeader first>
-                <H2>Exercises</H2>
-              </ListItem>
-              <ExerciseListEl exercise="Squat" />
-              <ExerciseListEl exercise="Benchpress" />
-              <ExerciseListEl exercise="Pullup" />
-              <ExerciseListEl exercise="Row" />
+          <Row style={{marginTop: 16}}>
+            <Left>
+              <H2>Exercises</H2>
+            </Left>
+            <Right>
+              <Button
+                primary
+                block
+                onPress={() => navigate('AddExercise')}
+              >
+                <Text> ADD EXERCISE </Text>
+              </Button>
+            </Right>
+          </Row>
+
+          <View>
+            <List
+              dataArray={this.state.exercises}
+              renderRow={(exercise) => 
+                <ExerciseListEl exercise={exercise.name} />
+              }
+            >
             </List>
-          </Form>
-          <Button
-            primary
-            large
-            block
-            onPress={() => navigate('AddExercise')}
-          >
-            <Text> ADD EXERCISE </Text>
-          </Button>
+          </View>
 
-          <Button
-            success
-            large
-            block
-            onPress={() => navigate('Home')}
-          >
-
-            <Text> SAVE SESSION </Text>
-          </Button>
-          
         </Content>
+        <Footer>
+          <FooterTab>
+            <Button
+              success
+              large
+              block
+              onPress={this.saveSession}
+            >
+              <Text> SAVE SESSION </Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     )
   }
