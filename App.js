491737d9b59dfc705665f9917e5312387d73f05e
Pederduel <pederduel@gmail.com>
2018-10-01 16:12:35 +0200

    #8 Pedometer showing below calendar

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import CalendarView from './components/CalendarView.js'
+import PedometerView from './components/PedometerView.js'
 
 export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
         <CalendarView />
+        <PedometerView />
       </View>
     )
   }
