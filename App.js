c8aafc3a77916e0d77cee725421a7a66559bb385
Dora Oline <doraoline@gmail.com>
2018-10-01 19:06:28 +0200

    Moved home to its own file

--- App.js
+++ App.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 
 import CalendarView from './src/components/CalendarView.js'
 import SessionForm from './src/components/SessionForm.js'
 import AddExercise from './src/components/AddExercise.js'
+import Home from './src/components/Home.js'
 
 import  { Button, Container, Header, Icon, StyleProvider, Root} from 'native-base'
 import getTheme from './native-base-theme/components'
 import material from './native-base-theme/variables/material'
 
 import { createStackNavigator } from 'react-navigation'
-
-class Home extends Component {
-  static navigationOptions = {
-    title: 'Home'
-  }
-
-  render() {
-    const { navigate } = this.props.navigation
-
-    return (
-      <View>
-        <CalendarView />
-        <Button 
-          primary
-          large
-          block
-          onPress={() => this.props.navigation.navigate('SessionForm')}
-        >
-          
-          <Text> ADD SESSION </Text>
-        </Button>
-      </View>
-
-    )
-  }
-}
 
 export default class App extends Component {
   render() {
--- src/components/__tests__/AddExercise.js
+++ src/components/__tests__/AddExercise.js
@@ -5,19 +5,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import AddExercise from '../AddExercise'
-
+import toJson from 'enzyme-to-json';
 
 console.log("Before test")
 it('renders correctly', () => {
-  console.log("In test")
-  //const wrapper = rendered.create(<AddExercise />)
-  //expect(wrapper).toMatchSnapshot()
+  const wrapper = shallow(<AddExercise />)
+  const component = wrapper.dive()
 
-  expect(1).toEqual(1)
+  expect(toJson(wrapper)).toMatchSnapshot()
 })
 
 it('calls addExercise when button is pressed', () => {
+  const wrapper = shallow(<AddExercise />)
+  const spy = jest.spyOn(wrapper.dive(), 'addExercise')
+  const component = wrapper.dive()
 
+  component.find('#addExerciseButton').simulate('click')
 
-
+  expect(spy).toHaveBeenCalled()
 })
