80c43a50ed727bf399a77b9b3e08f059c5f682ca
Dora Oline Eriksrud <doraoline@gmail.com>
2018-10-02 14:57:01 +0200

    Fixed rebase errors

--- App.js
+++ App.js
@@ -10,41 +10,14 @@ import CreateSession from './src/components/CreateSession.js'
 import AddExercise from './src/components/AddExercise.js'
 import Home from './src/components/Home.js'
 
-import  { Button, Container, Header, Icon, StyleProvider, Root} from 'native-base'
+import { Button, Container, Header, Icon, StyleProvider, Root} from 'native-base'
 import getTheme from './native-base-theme/components'
 import material from './native-base-theme/variables/material'
 
 import { createStackNavigator } from 'react-navigation'
 
-class Home extends Component {
-  static navigationOptions = {
-    title: 'Home'
-  }
-
-  render() {
-    const { navigate } = this.props.navigation
-
-    return (
-      <Provider store={store}>
-        <SafeAreaView style={styles.container}>
-          <CalendarView />
-          <Button 
-            primary
-            large
-            block
-            onPress={() => this.props.navigation.navigate('SessionForm')}
-          >
-            
-            <Text> ADD SESSION </Text>
-          </Button>
-        </SafeAreaView>
-      </Provider>
-    )
-  }
-}
-
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <Root>
         <StyleProvider style={getTheme(material)}>
@@ -69,8 +42,8 @@ const RootStack = createStackNavigator(
       },
       headerTintColor: '#fff',
       headerTitleStyle: {
-        fontWeight: 'bold' 
-      } 
+        fontWeight: 'bold'
+      }
     }
   }
 )
