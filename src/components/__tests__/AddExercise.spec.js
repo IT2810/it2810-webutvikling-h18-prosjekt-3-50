aeee0f0fa6aa6305e3cc78031fd518aceb26996f
Dora Oline <doraoline@gmail.com>
2018-10-06 15:08:38 +0200

    All AddExercise tests are passing

--- src/components/__tests__/AddExercise.spec.js
+++ src/components/__tests__/AddExercise.spec.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json';
 import AddExercise from '../AddExercise'
 import 'native-base'
@@ -25,39 +25,22 @@ describe('AddExercise', () => {
     expect(toJson(wrapper)).toMatchSnapshot()
   })
   
-
   describe('addExercise', () => {
-    /*it('calls addExercise when button is pressed', () => {
+    it('calls addExercise when button is pressed', () => {
       let addExerciseMock = jest.fn()
       let wrapper = shallow(<AddExercise />)
 
-      //AddExercise.prototype.addExercise = addExerciseMock
-      console.log("Before adding mocks")
-      //wrapper.instance().addExercise = addExerciseMock //jest.fn(() => console.log("Mock function"))
+      wrapper.instance().addExercise = addExerciseMock
       wrapper.instance().showToast = jest.fn()
-      console.log(wrapper.instance().addExercise)
-      console.log(addExerciseMock)
 
-
-      wrapper.update()
-      //let spy = jest.spyOn(wrapper.instance(), 'addExercise').mockImplementation(() => { console.log('second mock')})
-
-      //console.log(wrapper.instance().addExercise)
-
-      //wrapper.update()
-      let spy = jest.spyOn(wrapper.instance(), 'addExercise')
-
-      console.log(wrapper.instance().addExercise)
-      console.log(spy)
+      wrapper.instance().forceUpdate()
 
       let button = findByID(wrapper, 'addExerciseButton')
       button.props().onPress()
-      console.log("After press")
-      expect(spy).toHaveBeenCalled()
-      expect(wrapper.instance().addExercise).toHaveBeenCalled()
-      expect(addExerciseMock).toHaveBeenCalled()
+
+      expect(addExerciseMock.mock.calls.length).toBe(1)
     })
-*/
+
     it('navigates to SessionForm if validateExercise is true', () => {
       const navigateMock = jest.fn()
       const navigation = {navigate: navigateMock}
@@ -65,7 +48,6 @@ describe('AddExercise', () => {
 
       let validateExerciseMock = jest.fn()
       validateExerciseMock.mockReturnValue(true)
-
 
       wrapper.instance().validateExercise = validateExerciseMock
       wrapper.update()
@@ -82,7 +64,6 @@ describe('AddExercise', () => {
 
       let validateExerciseMock = jest.fn()
       validateExerciseMock.mockReturnValue(false)
-
 
       wrapper.instance().validateExercise = validateExerciseMock
       wrapper.update()
